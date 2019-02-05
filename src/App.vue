4e3b21172594d4457c85d04e8116b2c9c29f745d
Freek Wevers <freek_wevers@yahoo.com>
2019-02-05 20:27:19 +0100

    Initial commit
    Empty vue app created with @vue/cli

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <h1>My new Vue application</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "app"
+};
+</script>
+
+<style lang="scss">
+
+</style>
