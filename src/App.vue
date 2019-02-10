a6e6681bf2fd2beaba46916981a41809312363ab
Freek Wevers <freek_wevers@yahoo.com>
2019-02-10 16:43:51 +0100

    Rendered cards for different players

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,40 @@
                     <div class="main-container">
                         <div class="content-container">
                             <div class="section">
-                                <button class="btn btn--1" @click="dealCards()">Deal</button>
-                                <ul class="my-cards">
-                                    <app-card :card="card" v-for="card in gameDeck" :key="card.id"></app-card>
-                                </ul><!-- /.my-cards -->
+                                <button class="btn btn--1" @click="dealCards" v-if="!gameDeck">Deal</button>
+                                <div class="deck-container" v-else>
+                                    <h3>Deck</h3>
+                                    <ul class="deck">
+                                        <app-card :card="card" v-for="card in gameDeck" :key="card.id" @click.native="takeOneCard"></app-card>
+                                    </ul><!-- /.my-cards -->
+                                </div><!-- /.deck-container -->
+                                <ul class="players">
+                                    <li class="player">
+                                        <h3>My cards</h3>
+                                        <ul class="my-cards">
+                                            <app-card :card="card" v-for="card in players[0].cards" :key="card.id" @click.native="playCard"></app-card>
+                                        </ul><!-- /.my-cards -->
+                                    </li>
+                                    <li class="player">
+                                        <h3>Cards player 2</h3>
+                                        <ul class="my-cards">
+                                            <app-card :card="card" v-for="card in players[1].cards" :key="card.id" @click.native="playCard"></app-card>
+                                        </ul><!-- /.my-cards -->
+                                    </li><!-- /.player -->
+                                    <li class="player">
+                                        <h3>Cards player 3</h3>
+                                        <ul class="my-cards">
+                                            <app-card :card="card" v-for="card in players[2].cards" :key="card.id" @click.native="playCard"></app-card>
+                                        </ul><!-- /.my-cards -->
+                                    </li><!-- /.player -->
+                                    <li class="player">
+                                        <h3>Cards player 4</h3>
+                                        <ul class="my-cards">
+                                            <app-card :card="card" v-for="card in players[3].cards" :key="card.id" @click.native="playCard"></app-card>
+                                        </ul><!-- /.my-cards -->
+                                    </li><!-- /.player -->
+                                </ul><!-- /.player -->
+
                             </div><!-- /.section -->
                         </div><!-- /.content-container -->
                     </div><!-- /.main-container -->
@@ -85,10 +115,16 @@ export default {
             }
 
             return array;
+        },
+        takeOneCard() {
+            console.log('take one card');
+        },
+        payCard() {
+            console.log('play card');
         }
     },
     created() {
-        // Fill cards
+        // Fill cards array
         api.getCards('/data/deck.json').getAll()
         .then(response => {
             this.deck = response.data;
